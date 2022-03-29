2a2119b73e3983147169f7519a2b4d363ec56a01
Dustin Rouillard <code@dstn.to>
2022-03-29 16:57:56 -0600

    Set http port to environment

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,7 @@
 use Mix.Config
 
 config :lanyard,
+  http_port: String.to_integer(System.get_env("PORT") || "4001"),
   discord_spotify_activity_id: "spotify:1",
   command_prefix: System.get_env("COMMAND_PREFIX") || ".",
   bot_presence: System.get_env("BOT_PRESENCE") || "you <3",
